131f5b28df86af363fa799d6b11528033416542a
MuziUH <mn22abi@herts.ac.uk>
2026-03-10 00:51:27 +0000

    initial backend - express server with get, post, delete and file persistence

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+const fs = require("fs")
+app.use(express.json())
+
+function saveJobs() {
+fs.writeFileSync("jobs.json",JSON.stringify(jobs) )
+}
+
+function loadJobs(){
+    try {
+const data = fs.readFileSync("jobs.json","utf8")
+return JSON.parse(data)
+    } catch {
+        return[]
+    }
+}
+
+let jobs = loadJobs()
+
+app.get("/ping", (req, res) => {
+    res.json({message: "pong"})
+})
+
+app.get("/jobs", (req, res) => {
+    res.json(jobs)
+})
+
+app.post("/jobs", (req, res) => {
+    const job = req.body
+    job.id = Date.now().toString()
+    jobs.push(job)
+    saveJobs()
+    res.json(job)
+})
+
+app.delete("/jobs/:id", (req, res) => {
+    jobs = jobs.filter(job => job.id !== req.params.id)
+    saveJobs()
+    res.json({success: true})
+
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+})
