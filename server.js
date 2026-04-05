99015da09b6bf07591b98ec1d32598659881e7bd
MuziUH <mn22abi@herts.ac.uk>
2026-04-05 23:21:15 +0100

    connect Supabase database

--- server.js
+++ server.js
@@ -1,55 +1,73 @@
 const cors = require("cors")
 const express = require("express")
+const { createClient } = require("@supabase/supabase-js")
+
+const supabaseUrl = "https://xderhoeuvdpzfdupuezh.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZXJob2V1dmRwemZkdXB1ZXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMzUxOTYsImV4cCI6MjA5MDYxMTE5Nn0.mWCFHvlqOoUkIfY3fGEDwgoPkeZhit0zcgequYKjqvE"
+const supabase = createClient(supabaseUrl, supabaseKey)
 const app = express()
 app.use(cors())
-const fs = require("fs")
+= require("fs")
 app.use(express.json())
 
-function saveJobs() {
-fs.writeFileSync("jobs.json",JSON.stringify(jobs) )
-}
 
-function loadJobs(){
-    try {
-const data = fs.readFileSync("jobs.json","utf8")
-return JSON.parse(data)
-    } catch {
-        return[]
-    }
-}
-
-let jobs = loadJobs()
 
 app.get("/ping", (req, res) => {
     res.json({message: "pong"})
 })
 
-app.get("/jobs", (req, res) => {
-    res.json(jobs)
+app.get("/jobs", async (req, res) => {
+    const { data, error} = await supabase
+    .from("Jobs")
+    .select("*")
+    if(error) return res.status(500).json({error: error.message})
+    res.json(data)
 })
 
-app.post("/jobs", (req, res) => {
-    const job = req.body
-    job.id = Date.now().toString()
-    job.date = new Date().toLocaleDateString()
-    jobs.push(job)
-    saveJobs()
-    res.json(job)
+app.post("/jobs", async (req, res) => {
+    const { company, role, status} = req.body
+    if (!company || !role){
+        return res.status(400).json({ error: "Company and role are required. "})
+    }
+    const job = {
+        id: Date.now().toString(),
+        company,
+        role,
+        status: status || "Applied",
+        date: new Date().toLocaleDateString(),
+    }
+    const {data, error } = await supabase 
+    .from("Jobs")
+    .insert([job])
+    .select()
+    console.log("Supabase error:", error)  
+console.log("Supabase data:", data) 
+    if (error) return res.status (500).json({ error:error.message})
+        res.status(201).json(data[0])
 })
+    
 
-app.delete("/jobs/:id", (req, res) => {
-    jobs = jobs.filter(job => job.id !== req.params.id)
-    saveJobs()
+
+app.delete("/jobs/:id", async  (req, res) => {
+   const {error } = await supabase
+   .from("Jobs")
+   .delete()
+   .eq("id", req.params.id)
+   if (error) return res.status(500).json({error: error .message})
     res.json({success: true})
 
+   
 })
 
-app.put("/jobs/:id", (req, res) => {
-    const index = jobs.findIndex(job => job.id === req.params.id)
-    if (index === -1) return res.status(404).json({ error: "Job not found" })
-    jobs[index] = { ...jobs[index], ...req.body }
-    saveJobs()
-    res.json(jobs[index])
+app.put("/jobs/:id", async (req, res) => {
+    const { company, role, status } = req.body
+    const { data, error} = await supabase
+    .from("Jobs")
+    .update({ company, role, status})
+    .eq("id", req.params.id)
+    .select()
+    if (error) return res.status(500).json({ error:error.message})
+        res.json(data[0])
 })
 
 app.listen(3000, () => {
