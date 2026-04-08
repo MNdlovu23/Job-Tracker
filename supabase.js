6bbc8159bbd2a57dd6e0f166fb2ac3dca26b30b2
MuziUH <mn22abi@herts.ac.uk>
2026-04-08 11:09:40 +0100

    use enviroment variables for SUPABASE keys

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
-const supabaseUrl = "https://xderhoeuvdpzfdupuezh.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZXJob2V1dmRwemZkdXB1ZXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMzUxOTYsImV4cCI6MjA5MDYxMTE5Nn0.mWCFHvlqOoUkIfY3fGEDwgoPkeZhit0zcgequYKjqvE"
-
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
 
