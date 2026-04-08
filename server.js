c2a514f0416b35018d7a0d2bc2d1dedb4f101efb
MuziUH <mn22abi@herts.ac.uk>
2026-04-08 11:28:59 +0100

    add vercel config

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.put("/jobs/:id", async (req, res) => {
     if (error) return res.status(500).json({ error:error.message})
         res.json(data[0])
 })
-
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
