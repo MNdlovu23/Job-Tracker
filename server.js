cedf246780a31597007a239f390f98e16c78d571
MuziUH <mn22abi@herts.ac.uk>
2026-04-30 12:20:38 +0100

    fix: move API keys to environment variables

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require("dotenv").config()
 const cors = require("cors")
 const express = require("express")
 const { createClient } = require("@supabase/supabase-js")
 
-const supabaseUrl = "https://xderhoeuvdpzfdupuezh.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkZXJob2V1dmRwemZkdXB1ZXpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwMzUxOTYsImV4cCI6MjA5MDYxMTE5Nn0.mWCFHvlqOoUkIfY3fGEDwgoPkeZhit0zcgequYKjqvE"
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 const app = express()
 app.use(cors())
